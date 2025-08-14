e08454e977a7e23dd4687c67d54d95b4513980a7
Ben Clark <benclark@Bens-MacBook-Pro.local>
2025-08-14 15:28:49 +1000

    Add .gitignore and remove ignored files from tracking

--- dist/main/preload-wrapper.cjs
+++
@@ -1,8 +0,0 @@
-// CommonJS wrapper that dynamically imports the ESM preload.
-(async () => {
-    const path = require('path');
-    const { pathToFileURL } = require('url');
-    const esmFile = path.join(__dirname, 'preload.js'); // compiled preload
-    await import(pathToFileURL(esmFile).toString());
-  })();
-  
